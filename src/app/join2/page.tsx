9d232eba75d17dffa72ab3955140500c750052b8
softwarerbfl <32202018@dankook.ac.kr>
2023-09-01 10:08:09 +0900

    useRef를 적용한 회원 가입 구현

---
+++ src/app/join2/page.tsx
@@ -0,0 +1,80 @@
+"use client"
+import React, { useState, useRef, useCallback } from "react";
+function useJoin() {
+    const [id, setId] = useState<string>("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const confirmPassword = useRef<HTMLInputElement | null>(null);
+    const confirmEmail= useRef<HTMLInputElement | null>(null);
+    
+    //아이디는 그냥 통과
+    const onIdHandler = useCallback((event:any) => {
+        setId(event.currentTarget.value)
+    },[]);
+
+    const onEmailHandler = useCallback((event:any)=> {
+        setEmail(event.currentTarget.value)
+        
+        
+    },[]);
+    const onPasswordHandler = useCallback((event:any) => {
+        setPassword(event.currentTarget.value)
+        
+    },[]);
+    const onSubmit = useCallback((event:any) =>{
+        event.preventDefault(); // 페이지 새로고침 방지
+        // 비밀번호 부터 유효성 검증하고 실패 시 console 출력 및 커서 옮기기
+        var len = password.length;
+        var low=0; // 소문자 개수
+        var high=0; // 대문자 개수
+        var spc=0;  // 특수문자 개수
+        var pattern = /[~!@#$%^&*()_+|<>?:{}]/;	// 특수문자
+        for (var i=0;i<len;i++){
+            if(password[i].toLowerCase()==password[i]){
+                low+=1;
+            }
+            else if(password[i].toUpperCase()==password[i]){
+                high+=1;
+            }
+        }
+        if (len<8 || !pattern.test(password) || low==0 ||high==0){
+            console.log("비밀번호는 8자리 이상 대소문자, 특수문자로 구성되어야 합니다.");
+            confirmPassword.current?.focus();
+            alert("비번 실패");
+        }
+        else{
+            console.log("비밀번호 유효성 검사 완료")
+            var idx = email.indexOf('@')
+
+            //  @가 없거나 양 옆에 단어가 없는 경우
+            if (idx<=0 || idx>=email.length-1){
+                confirmEmail.current?.focus();
+            }
+            else{
+                // 이메일 유효성까지 통과
+                console.log("이메일 유효성 검사 완료")
+                console.log("회원가입 완료")
+            }
+        }
+        
+    } ,[email, password]);
+    return (
+        
+        <div className="bg-white">
+            <h1 className="font-['gmarket'] text-blue-600">회원가입</h1>
+            <form>
+                <label className="font-['gmarket'] text-blue-600">이름</label>
+                <div><input name="id" type="id" placeholder="이름" value={id} onChange={onIdHandler} className="focus:bg-sky-100 block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input></div>
+                <label className="font-['gmarket'] text-blue-600">비밀번호</label>
+                <div><input name="password" type="password" ref ={confirmPassword} placeholder="비밀번호" value={password} onChange={onPasswordHandler} className="focus:bg-sky-100 block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input></div>
+                <label className="font-['gmarket'] text-blue-600">이메일</label>
+                <div ><input name="email" type="email" ref={confirmEmail} placeholder="이메일" value={email} onChange={onEmailHandler} className="focus:bg-sky-100 block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input></div>
+                <div><button onClick={onSubmit} className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600" > Regist</button></div>
+                
+            </form>
+
+            
+        </div>
+    );
+};
+export default useJoin;
